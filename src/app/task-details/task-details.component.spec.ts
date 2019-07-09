33fe59d83fe4a513b0cafee06cf06d415bd60dff
HaritaJ <chiquita.prabhu@gmail.com>
2019-07-09 14:10:53 -0400

    add all

---
+++ src/app/task-details/task-details.component.spec.ts
@@ -0,0 +1,45 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContentModule } from '@alfresco/adf-content-services';
+import { ProcessModule } from '@alfresco/adf-process-services';
+import { CoreModule, TranslateLoaderService } from '@alfresco/adf-core';
+import { TaskDetailsComponent } from './task-details.component';
+import { AlfrescoApiServiceMock, AlfrescoApiService } from '@alfresco/adf-core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { PreviewService } from './../../app/services/preview.service';
+
+describe('TaskDetailsComponent', () => {
+    let component: TaskDetailsComponent;
+    let fixture: ComponentFixture<TaskDetailsComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                BrowserAnimationsModule,
+                RouterTestingModule,
+                CoreModule.forRoot(),
+                ContentModule.forRoot(),
+                ProcessModule.forRoot(),
+                TranslateModule.forRoot({
+                    loader: { provide: TranslateLoader, useClass: TranslateLoaderService }
+                })
+            ],
+            declarations: [TaskDetailsComponent],
+            providers: [
+                { provide: AlfrescoApiService, useClass: AlfrescoApiServiceMock },
+                PreviewService
+            ]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TaskDetailsComponent);
+        component = fixture.componentInstance;
+    });
+
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+    });
+});
